3412277bff90cc8a0295f9a00abff58e4542bbd9
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-07-27 02:17:36 +0300

    Add class Constants and Helpers for package 'utils', fix some string values

---
+++ app/src/main/java/com/udacity/vinhphuc/musicplayer/utils/Helpers.java
@@ -0,0 +1,111 @@
+package com.udacity.vinhphuc.musicplayer.utils;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.udacity.vinhphuc.musicplayer.R;
+
+/**
+ * Created by VINH PHUC on 26/7/2018
+ */
+public class Helpers {
+
+    public static void showAbout(AppCompatActivity activity) {
+        FragmentManager fm = activity.getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        Fragment prev = fm.findFragmentByTag("dialog_about");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        new AboutDialog().show(ft, "dialog_about");
+    }
+
+    public static String getATEKey(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean("dark_theme", false) ?
+                "dark_theme" : "light_theme";
+    }
+
+    public static class AboutDialog extends DialogFragment {
+
+        String urlLinkedin = "https://www.linkedin.com/in/phuc-truong-200296";
+        String urlGitHub = "https://github.com/thvphuc246";
+
+        public AboutDialog() {
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+            LinearLayout aboutBodyView = (LinearLayout) layoutInflater.inflate(R.layout.dialog_about, null);
+
+            TextView appVersion = (TextView) aboutBodyView.findViewById(R.id.app_version_name);
+
+            TextView linkedIn = (TextView) aboutBodyView.findViewById(R.id.linkedin);
+            TextView github = (TextView) aboutBodyView.findViewById(R.id.github);
+
+            TextView dismiss = (TextView) aboutBodyView.findViewById(R.id.dismiss_dialog);
+            dismiss.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dismiss();
+                }
+            });
+            linkedIn.setPaintFlags(linkedIn.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+            github.setPaintFlags(github.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+            linkedIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(urlLinkedin));
+                    startActivity(i);
+                }
+
+            });
+            github.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(urlGitHub));
+                    startActivity(i);
+                }
+
+            });
+            try {
+                PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+                String version = pInfo.versionName;
+                int versionCode = pInfo.versionCode;
+                appVersion.setText("Udacity Music Player " + version);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            return new AlertDialog.Builder(getActivity())
+                    .setView(aboutBodyView)
+                    .create();
+        }
+
+    }
+
+}
