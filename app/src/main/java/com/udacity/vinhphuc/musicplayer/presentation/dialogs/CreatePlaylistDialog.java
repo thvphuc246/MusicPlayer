dc4cd2190ec76ee3b67841f2b27a0c4c4ebfbf96
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-08-17 17:35:27 +0300

    Add Dialog classes

--- app/src/main/java/com/udacity/vinhphuc/musicplayer/presentation/dialogs/CreatePlaylistDialog.java
+++ app/src/main/java/com/udacity/vinhphuc/musicplayer/presentation/dialogs/CreatePlaylistDialog.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.udacity.vinhphuc.musicplayer.MusicPlayer;
+import com.udacity.vinhphuc.musicplayer.R;
 import com.udacity.vinhphuc.musicplayer.data.model.Song;
 import com.udacity.vinhphuc.musicplayer.presentation.fragments.PlaylistFragment;
 
@@ -41,7 +42,7 @@ public class CreatePlaylistDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new MaterialDialog.Builder(getActivity()).positiveText("Create").negativeText("Cancel").input("Enter playlist name", "", false, new MaterialDialog.InputCallback() {
+        return new MaterialDialog.Builder(getActivity()).positiveText(getString(R.string.create)).negativeText(getString(R.string.cancel)).input(getString(R.string.enter_playlist_name), "", false, new MaterialDialog.InputCallback() {
             @Override
             public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
 
@@ -52,12 +53,12 @@ public class CreatePlaylistDialog extends DialogFragment {
                     if (songs != null && songs.length != 0)
                         MusicPlayer.addToPlaylist(getActivity(), songs, playistId);
                     else
-                        Toast.makeText(getActivity(), "Created playlist", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), R.string.created_playlist, Toast.LENGTH_SHORT).show();
                     if (getParentFragment() instanceof PlaylistFragment) {
                         ((PlaylistFragment) getParentFragment()).updatePlaylists(playistId);
                     }
                 } else {
-                    Toast.makeText(getActivity(), "Unable to create playlist", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), R.string.unable_creating_playlist, Toast.LENGTH_SHORT).show();
                 }
 
             }
